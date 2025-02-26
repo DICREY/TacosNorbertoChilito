18dd2ba5fd39832cecd79114d2b98a139bb61bad
Dicrey <dicreyraptor@gmail.com>
2025-02-26 15:36:53 -0500

    1.1.6

--- prueba1.py
+++ prueba1.py
@@ -1,6 +1,7 @@
 import customtkinter as ctk
-from colores import *
+from vars import *
 from PIL import Image
+import os
 
 #Declaracion Ventana
 ventana = ctk.CTk()
@@ -9,8 +10,7 @@ ventana.title("Hola pe causa")
 ventana.geometry("1280x720")
 ventana.resizable(width=True,height =True)
 
-user="Kevin"
-psw="123"
+
 
 #Validacion de usuario de prueba
 
@@ -28,28 +28,26 @@ def validar():
 frame1= ctk.CTkFrame(master=ventana)
 frame1.configure(width=600,
                 height=200,
-                border_width=1,
+                border_width=0,
                 fg_color=fondAma,
                 corner_radius=0
                 )
 frame1.pack(side="left",expand=False, fill="both")
 
-logo = ctk.CTkImage(light_image=Image.open("assets/logo.png"),
-                    dark_image=Image.open("assets/logo.png"),
-                    size=(400,300))
+logo = ctk.CTkImage(light_image=Image.open("assets/logo.png"),size=(400,300))
 
 logoIn=ctk.CTkLabel(frame1, text="", image=logo, width=200, height=200)
 logoIn.place(relx=0.5, rely=0.45, anchor="center")
 
-titleLog= ctk.CTkLabel(frame1, text="T.C.N.S. Demo version 0.1", font=("",30,"bold"),text_color=btnCafe)
+titleLog= ctk.CTkLabel(frame1, text="T.C.N.S. Demo version 0.1", font=('Helvetica',25,'bold'),text_color=btnCafe)
 titleLog.place(relx=0.5, rely=0.7, anchor="center")
 
 #Frame de la derecha
 
-frame2= ctk.CTkFrame(master=ventana)
+frame2 = ctk.CTkFrame(master=ventana)
 frame2.configure(width=300,
                 height=720,
-                border_width=1,
+                border_width=0,         
                 fg_color=fondoGr,
                 corner_radius=0
                 )
@@ -57,32 +55,37 @@ frame2.pack(side="right",expand=True, fill="both")
 
 title = ctk.CTkLabel(frame2,
                      text="Login",
-                     font=("",40,"bold"),
+                     font=title_letter,
                      text_color=btnCafe)
 title.place(relx=0.5,rely=0.26,anchor="center")
 
 
 titleUser = ctk.CTkLabel(frame2,
                      text="Nombre Usuario",
-                     font=("",20,"bold"),
+                     font=letter,
                      text_color=btnCafe)
 titleUser.place(relx=0.35,rely=0.356,anchor="w")
 
 
 titlePsw = ctk.CTkLabel(frame2,
                      text="Contraseña",
-                     font=("",20,"bold"),
+                     font=letter,
                      text_color=btnCafe)
 titlePsw.place(relx=0.35,rely=0.456,anchor="w")
 
 userInput = ctk.CTkEntry(frame2,
                          placeholder_text="Usuario",
+                         border_width=0,
                          height=40,
+                         font=letter,
                          fg_color=blanco)
 userInput.place(relx=0.5,rely=0.4, anchor="center", relwidth=0.3)
 
 pswInput = ctk.CTkEntry(frame2,
                          placeholder_text="Contraseña",
+                         placeholder_text_color=gris,
+                         font=letter,
+                         border_width=0,
                          height=40,
                          fg_color=blanco)
 pswInput.place(relx=0.5,rely=0.5, anchor="center", relwidth=0.3)
@@ -90,6 +93,7 @@ pswInput.place(relx=0.5,rely=0.5, anchor="center", relwidth=0.3)
 btnLog = ctk.CTkButton(frame2,
                        text="Ingresar",
                        height=40,
+                       font=letter,
                        fg_color=btnCafe,
                        hover_color=btnHoover,
                        command=validar)

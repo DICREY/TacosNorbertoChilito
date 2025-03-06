6e8694a35d890e294d20fe2cf93b428c52b75d20
Dicrey <dicreyraptor@gmail.com>
2025-03-05 19:14:01 -0500

    1.1.23

---
+++ data/proc_almc/orders.sql
@@ -0,0 +1,89 @@
+CREATE PROCEDURE DesactiveOrder(
+    -- Procedure Vars
+    IN p_var_search VARCHAR(100)
+)
+BEGIN
+    -- Manejador de excepciones
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION 
+    BEGIN
+        ROLLBACK;
+        RESIGNAL;
+    END;
+
+    SET autocommit = 0;
+
+    START TRANSACTION;
+    
+    -- Update Pedidos for desactive it
+    UPDATE pedidos p
+    SET p.estado = "ANULADO"
+    WHERE p.fec_ped = p_var_search;
+
+    COMMIT;
+
+    SET autocommit = 1;
+END //
+
+CREATE PROCEDURE SearchOrderPendings()
+BEGIN
+    -- Update Pedidos for desactive it
+    SELECT
+        p.fec_ped,
+        pe.nom_per,
+        pe.ape_per,
+        pe.cel_per,
+        pa.nom_pai,
+        p.obs_ped
+    FROM pedidos p,clientes c,personas pe,paises pa
+    WHERE
+        c.id_cli = p.id_cli AND
+        c.id_cli = pe.id_per AND
+        pe.pais_per = pa.id_pai AND
+        p.estado = "PENDIENTE"
+    LIMIT 50;
+END //
+
+CREATE PROCEDURE SearchOrderDelivered()
+BEGIN
+    -- Update Pedidos for desactive it
+    SELECT
+        p.fec_ped,
+        pe.nom_per,
+        pe.ape_per,
+        pe.cel_per,
+        pa.nom_pai,
+        p.obs_ped
+    FROM pedidos p,clientes c,personas pe,paises pa
+    WHERE
+        c.id_cli = p.id_cli AND
+        c.id_cli = pe.id_per AND
+        pe.pais_per = pa.id_pai AND
+        p.estado = "ENTREGADO"
+    LIMIT 50;
+END //
+CREATE PROCEDURE SearchOrder(
+    IN var_order VARCHAR(100)
+)
+BEGIN
+    -- Update Pedidos for desactive it
+    SELECT
+        p.fec_ped,
+        pe.nom_per,
+        pe.ape_per,
+        pe.cel_per,
+        pa.nom_pai,
+        p.obs_ped
+    FROM pedidos p,clientes c,personas pe,paises pa
+    WHERE
+        c.id_cli = p.id_cli AND
+        c.id_cli = pe.id_per AND
+        pe.pais_per = pa.id_pai AND
+        p.fec_ped = var_order AND
+        p.estado = "PENDIENTE" OR
+        c.id_cli = p.id_cli AND
+        c.id_cli = pe.id_per AND
+        pe.pais_per = pa.id_pai AND
+        p.fec_ped = var_order AND
+        p.estado = "ENTREGADO"
+    LIMIT 50;
+END //

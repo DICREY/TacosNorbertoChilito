41b20007390fff42ef98b07671437667ba97ecd2
Dicrey <dicreyraptor@gmail.com>
2025-03-06 19:37:14 -0500

    1.1.24

--- data/proc_almc/inventario.sql
+++ data/proc_almc/inventario.sql
@@ -6,12 +6,21 @@ BEGIN
         E.can_exi_mat AS existencia_actual,
         SUM(C.can_com) AS cantidad_comprada,
         (E.can_exi_mat + SUM(C.can_com)) AS nueva_existencia
-    FROM 
-        MATERIALES M
-    JOIN 
-        EXISTENCIAS_MATERIALES E ON M.id_mat = E.id_mat
-    JOIN 
-        COMPRAS C ON M.id_mat = C.id_mat
+    FROM MATERIALES M, EXISTENCIAS_MATERIALES E, COMPRAS C
+    WHERE 
+        M.id_mat = E.id_mat AND
+        M.id_mat = C.id_mat
     GROUP BY 
         M.id_mat;
+END //
+CREATE PROCEDURE inventoryMachinery()
+BEGIN
+    SELECT 
+        nom_maq,
+        can_exi_maq
+    FROM maquinaria 
+    WHERE
+        estado = "FUNCIONAL"
+    GROUP BY 
+        id_maq;
 END //
